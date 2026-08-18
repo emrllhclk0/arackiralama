435742cc2c8e92d6e872ff58c55050579cd06ea2
Emrullah Celik <emrllhclk@example.com>
2026-08-18 21:38:05 +0300

    Deploy web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2255590968" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1842463078" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
